5c956dd45c46f5f8f0b41a374b4c294fb0ab576b
dash <daobandoso@est.utn.ac.cr>
2024-08-01 16:35:26 -0600

    Clase Task
    Se crea la clase Task

---
+++ Taks/Task.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taks;
+
+import Comentarios.Commet;
+import Users.User;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author dashs
+ */
+public class Task {
+    private int id;
+    private String description;
+    private boolean complet;
+    private User user;
+    private ArrayList<Commet> coment;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isComplet() {
+        return complet;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public ArrayList<Commet> getComent() {
+        return coment;
+    }
+
+    public void setComplet(boolean complet) {
+        this.complet = complet;
+    }
+
+    public Task(int id, String description, String complet, User user) {
+        this.id = id;
+        this.description = description;
+        this.complet = false;
+        this.user = user;
+        this.coment = new ArrayList();
+    }
+    
+    public Task(){
+        this(0,"",new User(),new ArrayList());
+    }
+    
+    public void addComment(){
+        listcomment.add(1);
+        
+    }
+    
+}
