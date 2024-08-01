e08813fd0efa91476d461ff2895281695e9bafc9
dash <daobandoso@est.utn.ac.cr>
2024-08-01 15:51:30 -0600

    Clase Users 2
    Se modifica la clase Users

--- Users/User.java
+++ Users/User.java
@@ -9,5 +9,35 @@ package Users;
  * @author dashs
  */
 public class User {
+    private String id;
+    private String name;
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public User() {
+        this("","","");
+    }
+    
     
 }
